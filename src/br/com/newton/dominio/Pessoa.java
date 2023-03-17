339902d25839030112c2b709d1878e22802fdd45
12111280 <Benfica.infantaria@gmail.com>
2023-03-17 19:59:51 -0300

    Feat:Cria projeto pessoa

---
+++ src/br/com/newton/dominio/Pessoa.java
@@ -0,0 +1,16 @@
+package br.com.newton.dominio;
+
+public class Pessoa {
+    private String nome;
+
+    public String validarDocumento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
